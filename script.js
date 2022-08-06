5aff2077df35afdd0a2eec0d58443d029e13419a
CKline22 <CKline.1022@gmail.com>
2022-08-06 12:55:15 -0400

    score needs work

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
-//let score = document.querySelectorAll('.score')
+let score = document.querySelectorAll('.score')
 let box = document.querySelectorAll('.box')
  box = Array.from(box)
 
  let player = "x"
 
- //let score = 0;
+ let gameScore = 0
 
-//winningConditions []
-//checkWin function () {
-    //winningConditions.forEach(function(condition){
-        //let check = condition.every(index => box[index].innerText == player)
-        //if (check){
-            //alert(player + "Has Won!")
-            //score ++
-        //}
-    //})
-//}
+ function currentScore () {
+    let playerScore = score.innerText + gameScore
+ }
+
+winningConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+function checkWin () {
+    winningConditions.forEach(function(condition){
+        let check = condition.every(index => box[index].innerText == player)
+        if (check){
+            alert(player + "Has Won!")
+            gameScore ++
+        }
+    })
+}
 
  box.forEach(function(box){
      box.addEventListener('click', function(){
         if (box.innerText != "") return
          box.innerText = player
-         //checkWin()
+         checkWin()
          player = player == "x" ? "o" : "x"
      })
  })
