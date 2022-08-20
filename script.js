6c61565d35f1561b574e750fa4c06cbcfb25c003
CKline22 <CKline.1022@gmail.com>
2022-08-19 21:48:50 -0400

    files formated

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-let box = document.querySelectorAll('.box')
- box = Array.from(box)
+let box = document.querySelectorAll(".box");
+box = Array.from(box);
 
- let player = "x"
+let player = "x";
 
 winningConditions = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-]
-function checkWin () {
-    winningConditions.forEach(function(condition){
-        let check = condition.every(index => box[index].innerText == player)
-        if (check){
-            alert(player + "Has Won!")
-        }
-    })
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+function checkWin() {
+  winningConditions.forEach(function (condition) {
+    let check = condition.every((index) => box[index].innerText == player);
+    if (check) {
+      alert(player + "Has Won!");
+    }
+  });
 }
 
- box.forEach(function(box){
-     box.addEventListener('click', function(){
-        if (box.innerText != "") return
-         box.innerText = player
-         checkWin()
-         player = player == "x" ? "o" : "x"
-     })
- })
+box.forEach(function (box) {
+  box.addEventListener("click", function () {
+    if (box.innerText != "") return;
+    box.innerText = player;
+    checkWin();
+    player = player == "x" ? "o" : "x";
+  });
+});
